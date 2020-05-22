0d09fcdb2c5b85852b8e0ac88a3f30d4307b2313
shonburgundy <seanbeloussow@yahoo.com>
2020-05-22 14:23:12 -0700

    pass preject creation path int test

--- app.rb
+++ app.rb
@@ -7,4 +7,17 @@ require('pry')
 also_reload('lib/**/*.rb')
 require("pg")
 
-DB = PG.connect({:dbname => "record_store"})
+DB = PG.connect({:dbname => "volunteer_tracker"})
+
+get('/') do
+  @projects = Project.all
+  erb(:homepage)
+end
+
+post('/projects') do
+  title = params[:title]
+  project = Project.new({:title => title, :id => nil})
+  project.save()
+  @projects = Project.all()
+  erb(:homepage)
+end
