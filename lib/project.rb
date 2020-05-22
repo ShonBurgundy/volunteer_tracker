0c16344ce1348e2f02494915c7ea35688c6b7fe2
shonburgundy <seanbeloussow@yahoo.com>
2020-05-22 10:22:03 -0700

    pass save method test in project.rb

--- lib/project.rb
+++ lib/project.rb
@@ -20,7 +20,12 @@ class Project
   end
 
   def save
-    
+    result = DB.exec("INSERT INTO projects (title) VALUES ('#{@title}') RETURNING id;")
+    @id = result.first().fetch("id").to_i
+  end
+
+  def ==(project_to_compare)
+    @title == project_to_compare.title
   end
 
 end
