4b80508cc18abad714e6653864ded0f08a5d46f1
shonburgundy <seanbeloussow@yahoo.com>
2020-05-22 11:08:40 -0700

    pass name, project_id & == method tests in volunteer spec

--- spec/volunteer_spec.rb
+++ spec/volunteer_spec.rb
@@ -1,28 +1,28 @@
-# require "spec_helper"
+require "spec_helper"
 
-# describe('#Volunteer') do
+describe('#Volunteer') do
 
-#   describe('#name') do
-#     it 'returns the name of the volunteer' do
-#       test_volunteer = Volunteer.new({:name => 'Jane', :project_id => 1, :id => nil})
-#       expect(test_volunteer.name).to eq 'Jane'
-#     end
-#   end
+  describe('#name') do
+    it 'returns the name of the volunteer' do
+      test_volunteer = Volunteer.new({:name => 'Jane', :project_id => 1, :id => nil})
+      expect(test_volunteer.name).to(eq('Jane'))
+    end
+  end
 
-#   describe('#project_id') do
-#     it('returns the project_id of the volunteer') do
-#       test_volunteer = Volunteer.new({:name => 'Jane', :project_id => 1, :id => nil})
-#       expect(test_volunteer.project_id).to(eq({1}))
-#     end
-#   end
+  describe('#project_id') do
+    it('returns the project_id of the volunteer') do
+      test_volunteer = Volunteer.new({:name => 'Jane', :project_id => 1, :id => nil})
+      expect(test_volunteer.project_id).to(eq(1))
+    end
+  end
 
-#   describe('#==') do
-#     it('checks for equality based on the name of a volunteer') do
-#       volunteer1 = Volunteer.new({:name => 'Jane', :project_id => 1, :id => nil})
-#       volunteer2 = Volunteer.new({:name => 'Jane', :project_id => 1, :id => nil})
-#       expect(volunteer1 == volunteer2).to(eq(true))
-#     end
-#   end
+  describe('#==') do
+    it('checks for equality based on the name of a volunteer') do
+      volunteer1 = Volunteer.new({:name => 'Jane', :project_id => 1, :id => nil})
+      volunteer2 = Volunteer.new({:name => 'Jane', :project_id => 1, :id => nil})
+      expect(volunteer1 == volunteer2).to(eq(true))
+    end
+  end
 
 #   describe('.all') do
 #     it('is empty to start') do
@@ -56,4 +56,4 @@
 #       expect(Volunteer.find(volunteer1.id)).to(eq(volunteer1))
 #     end
 #   end
-# end
+end
